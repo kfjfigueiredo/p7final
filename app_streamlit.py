2707c3b0a5204e00bf77e36ccdd7a993c265ed00
kfjfigueiredo <86651972+kfjfigueiredo@users.noreply.github.com>
2022-06-08 23:08:55 +0200

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -7,7 +7,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib 
-from lightgbm import LGBMClassifier
+import lightgbm
 
 import plotly.express as px
 import plotly.graph_objects as go
