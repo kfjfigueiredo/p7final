b33e3f7b6da6a0755427183dcef5a91ef8e9d330
kfjfigueiredo <86651972+kfjfigueiredo@users.noreply.github.com>
2022-06-08 23:04:11 +0200

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -20,7 +20,7 @@ st.set_page_config(page_title='Dashboard Scoring Credit - Prêt à dépenser ', 
 # Telecharger la base de données et les modèles
 df = joblib.load('df_complet.pkl') #df complet
 train_dataset = joblib.load('train_dataset.pkl')
-model_lgbm = joblib.load('train_dataset.pkl')
+model_lgbm = joblib.load('lgbm_model.pkl')
 img =Image.open('Logo_pad.PNG')
 graphique_shap_importance = Image.open('Shap_importance.png')
 y = df["target_reel"]
