7e8a4b9abe7fdfa861f767b632e89bce6d39c34a
kfjfigueiredo <86651972+kfjfigueiredo@users.noreply.github.com>
2022-06-10 16:26:59 +0200

    Update app_teste_10_06.py

--- app_teste_10_06.py
+++ app_teste_10_06.py
@@ -76,7 +76,7 @@ dt = probability
 dt["type_de_client"] = "p"
 dt["type_de_client"] = np.where((dt['1']>= 0.48), "client à risque", dt['type_de_client'])
 dt['type_de_client'] = np.where((dt['1']<0.48), "client peu risqué", dt['type_de_client'])
-dt = dt[['type_de_client']]
+dt = dt['type_de_client']
 
 chaine2 = '**type de client :**' + dt['type_de_client']
 
