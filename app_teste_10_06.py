4f16ef78f2637d2267303a23fd9bfd05cc9b25c9
kfjfigueiredo <86651972+kfjfigueiredo@users.noreply.github.com>
2022-06-10 11:36:17 +0200

    Update app_teste_10_06.py

--- app_teste_10_06.py
+++ app_teste_10_06.py
@@ -57,9 +57,11 @@ t2.title("Dashboard Scoring Credit ") # Titre du dashboard
 #df['SK_ID_CURR'] = df['SK_ID_CURR'].astype(str) # transformer l'ID en string 
 id_client = st.selectbox('Selectionnez un Id client', df.index, help = 'Choisissez un seul id client')
 
-id = int(id)
+train_dataset = joblib.load('train_dataset.pkl')   
+train_dataset['SK_ID_CURR'] = train_dataset.index
 X = train_dataset[train_dataset['SK_ID_CURR'] == id]
 probability = lgbm_model.predict_proba(X)[:,1]
+
 st.write('Probabilité de defaut de paiement:', str(round(probability *100)) +'%')
                   
 #affichage de la prédiction
