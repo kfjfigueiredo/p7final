6438f46d1f1d3cd717c71f65e47446307b617681
kfjfigueiredo <86651972+kfjfigueiredo@users.noreply.github.com>
2022-06-10 13:07:10 +0200

    Update app_teste_10_06.py

--- app_teste_10_06.py
+++ app_teste_10_06.py
@@ -69,13 +69,12 @@ probability = pd.DataFrame(probability, columns= ["0", "1"], index= df.index)
 probability["id_client"] = probability.index
 probability = probability[["id_client", "1"]]
 
-
 prob = probability[(probability["id_client"] ==id_client) & (probability["1"])]
 #st.write('Probabilité de defaut de paiement:', str(round(prob*100)) +'%')
 #chaine = '**Probabilité de défaut de payement:**' + str(round(prob*100)) + '%')
 
 
-chaine = '**Risque de défaut de payement :**' + str(prob) + '% de risque de défaut'
+chaine = '**Risque de défaut de payement :**' + str(prob*100) + '% de risque de défaut'
 st.markdown(chaine)
                  
 #affichage de la prédiction
