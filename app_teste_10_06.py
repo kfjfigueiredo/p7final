4c3d51eead4cea6f11fc02075edf6359cbb12a0f
kfjfigueiredo <86651972+kfjfigueiredo@users.noreply.github.com>
2022-06-10 17:02:20 +0200

    Update app_teste_10_06.py

--- app_teste_10_06.py
+++ app_teste_10_06.py
@@ -82,7 +82,7 @@ type_de_client = dt[(dt['id_client']==id_client) & (dt['type_de_client'])]
 if dt["type_de_client"] == "client à risque":
           chaine2 == '**type de client :**' + 'client à risque'
           
-else:
+if dt["type_de_client"] == "client peu risqué":
           chaine2 == '**type de client :**' + 'client peu risqué'
           
 #chaine2 = '**type de client :**' + str(type_de_client_b)
